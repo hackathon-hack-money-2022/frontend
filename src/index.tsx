1f08e39618fd3558188de5b90414df36d3f251e9
2xic <5640782+2xic@users.noreply.github.com>
2022-05-21 21:02:08 +0200

    Added push to github pages

--- src/index.tsx
+++ src/index.tsx
@@ -3,16 +3,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.Fragment>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.Fragment>
 );
 
